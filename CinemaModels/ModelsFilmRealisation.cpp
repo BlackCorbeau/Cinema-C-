11761a94f1acad8d896e44d40e55cc41062b736c
Remizov_Kirill <woron0987654321@gmail.com>
2024-05-24 21:15:27 +0300

    Дописаны методы для класса Film

--- CinemaModels/ModelsFilmRealisation.cpp
+++ CinemaModels/ModelsFilmRealisation.cpp
@@ -23,7 +23,15 @@ Film::Film(Film& f)
 	CostForTicket = f.CostForTicket;
 	liveBefore = f.liveBefore;
 }
-
+Film::Film(CTime timeofgoes, float rate, CString name, CString type, int costforticket, CDate livebefore)
+{
+	TimeOfGoes = timeofgoes;
+	Rate = rate;
+	Name = name;
+	this->type = type;
+	CostForTicket = costforticket;
+	liveBefore = livebefore;
+}
 Film::~Film()
 {
 	Name.~CString();
@@ -38,4 +46,54 @@ CTime Film::getTimeOfGoes()
 void Film::setTimeOfGoes(CTime T)
 {
 	TimeOfGoes = T;
+}
+
+float Film::getRate()
+{
+	return Rate;
+}
+
+void Film::setRate(float R)
+{
+	Rate = R;
+}
+
+CString Film::getName()
+{
+	return Name;
+}
+
+void Film::setName(CString str)
+{
+	Name = str;
+}
+
+CString Film::getType()
+{
+	return type;
+}
+
+void Film::setType(CString str)
+{
+	type = str;
+}
+
+int Film::getCostForTicket()
+{
+	return CostForTicket;
+}
+
+void Film::setCostForTicket(int cost)
+{
+	CostForTicket = cost;
+}
+
+CDate Film::getliveBefore()
+{
+	return liveBefore;
+}
+
+void Film::setliveBefore(CDate date)
+{
+	liveBefore = date;
 }
